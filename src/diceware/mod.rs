f5de955f2a1547206c08b85b78258b4f27eb2b99
James Pharaoh <james@pharaoh.uk>
2021-11-18 09:56:54 +0100

    add gen-diceware, clean up various things

---
+++ src/diceware/mod.rs
@@ -0,0 +1,3 @@
+mod eff_long_1;
+
+pub use eff_long_1::EFF_LONG_1;
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
+use rand::prelude::*;
+use rand::distributions::Uniform;
 use std::char;
 use std::env;
+use std::iter;
 use std::process;
 use std::path::Path;
 
-use rand;
-use rand::distributions::Distribution;
-use rand::distributions::Uniform;
+mod diceware;
 
 fn main () {
 
@@ -15,28 +16,43 @@ fn main () {
 
 	match command_name {
 
-		"gen-password" => {
-
-			let uniform = Uniform::from ('a' as u32 .. 'z' as u32);
-			let mut password = String::new ();
+		"gen-diceware" => {
+			let num_words = match args.len () {
+				1 => 6,
+				2 => args [1].parse ().unwrap (),
+				_ => panic! (),
+			};
+			if num_words < 1 { panic! () }
+			let word_list = diceware::EFF_LONG_1;
 			let mut rng = rand::thread_rng ();
-
-			for _ in 0 .. 20 {
-				let ch = char::from_u32 (uniform.sample (& mut rng)).unwrap ();
-				password.push (ch);
-			}
-
+			let words: Vec <& str> = word_list.choose_multiple (& mut rng, num_words)
+				.copied ().collect ();
+			let password = words.join (" ");
 			println! ("{}", password);
+		},
 
+		"gen-password" => {
+			let num_chars = match args.len () {
+				1 => 20,
+				2 => args [1].parse ().unwrap (),
+				_ => panic! (),
+			};
+			let mut rng = rand::thread_rng ();
+			let uniform = Uniform::from ('a' as u32 .. 'z' as u32);
+			let password: String = iter::repeat_with (
+				|| char::from_u32 (uniform.sample (& mut rng)).unwrap (),
+			).take (num_chars).collect ();
+			println! ("{}", password);
 		},
 
 		_ => {
-
-			eprintln! ("Program name not recognised: {}", command_name);
-
+			eprintln! ("Please invoke via a symlink:");
+			eprintln! ();
+			eprintln! ("  gen-diceware ...  Diceware words");
+			eprintln! ("  gen-password ...  Lowercase ASCII characters");
+			eprintln! ();
 			process::exit (1);
-
-		}
+		},
 
 	}
 
